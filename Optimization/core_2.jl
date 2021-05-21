20921cfecb989d5261e5670d3a332c686054f876
dbelivanis <dbelivan@stanford.edu>
2021-05-21 14:30:52 +0100

    check the lbfg algo

---
+++ Optimization/core_2.jl
@@ -0,0 +1,33 @@
+include("./param.jl");
+include("./aux_functions.jl");
+
+using Main.aux_functions
+using Main.param
+using ADCME
+using SparseArrays
+
+using DelimitedFiles
+using Dates
+using Plots
+
+maxiter = parse(Int64,ARGS[1])
+N_steps = parse(Int64,ARGS[2])
+print("maximum iteration: ",maxiter,"\t","Number of steps: ",N_steps,"\n")
+param_model_val = param_model(N_steps=N_steps);
+tf_variables, h_t, q_t_x, q_t_y = Darcy_flow_solver(param_model_val);
+
+loss,dw_2, opt_ADAM, opt_LFGS, opt_ADAM_sum, opt_LFGS_sum, diff_eval,p_pre_soft_max, p = Info_upscale(tf_variables,param_model_val,q_t_x, q_t_y,maxiter)
+
+sess = Session(); init(sess);
+
+
+global N_k_dis_ = 4
+global T_exp = -2
+T_=  10.0 .^ -T_exp
+
+save_values(sess,param_model_val,tf_variables,q_t_x, q_t_y,p,"w")
+
+print_status(sess,loss,diff_eval,T_exp,T_,N_k_dis_,tf_variables)
+# ScipyOptimizerMinimize(sess, opt_LFGS_sum,feed_dict = Dict(tf_variables.lambda => ones(1)*T_,tf_variables.N_k_dis=>8))
+BFGS!(sess,dw_2*1e5,options=Dict("maxiter"=> maxiter, "ftol"=>1e-18, "gtol"=>1e-18))
+

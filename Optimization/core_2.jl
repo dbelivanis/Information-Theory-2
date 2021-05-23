9d03e13c0684679a2da109a5f78f15a08dc547ce
dbelivanis <dbelivan@stanford.edu>
2021-05-23 15:12:46 +0100

    update the core_2

--- Optimization/core_2.jl
+++ Optimization/core_2.jl
@@ -31,4 +31,30 @@ print_status(sess,loss,diff_eval,T_exp,T_,N_k_dis_,tf_variables)
 # ScipyOptimizerMinimize(sess, opt_LFGS_sum,feed_dict = Dict(tf_variables.lambda => ones(1)*T_,tf_variables.N_k_dis=>8))
 BFGS!(sess,dw_2*1e5,options=Dict("maxiter"=> maxiter, "ftol"=>1e-18, "gtol"=>1e-18))
 
-save_values(sess,param_model_val,tf_variables,q_t_x, q_t_y,p,"w")
+save_values(sess,param_model_val,tf_variables,q_t_x, q_t_y,p,"a")
+print_status(sess,loss,diff_eval,T_exp,T_,N_k_dis_,tf_variables)
+
+check_diff = run(sess,diff_eval,feed_dict = Dict(tf_variables.lambda => ones(1)*T_,tf_variables.N_k_dis=>N_k_dis_))
+
+T_exp_final =4
+
+while T_exp <= T_exp_final
+
+    global T_=  10.0 ^ (-T_exp) 
+
+    print_status(sess,loss,diff_eval,T_exp,T_,N_k_dis_,tf_variables)
+    check_diff_ = run(sess,diff_eval,feed_dict = Dict(tf_variables.lambda => ones(1)*T_,tf_variables.N_k_dis=>N_k_dis_))  
+    print_status(sess,loss,diff_eval,T_exp,T_,N_k_dis_,tf_variables)
+    ScipyOptimizerMinimize(sess, opt_LFGS,feed_dict = Dict(tf_variables.lambda => ones(1)*T_,tf_variables.N_k_dis=>N_k_dis_))
+    print_status(sess,loss,diff_eval,T_exp,T_,N_k_dis_,tf_variables)
+    if round(T_exp,digits=2)%1 == 0
+
+        save_values(sess,param_model_val,tf_variables,q_t_x, q_t_y,p)
+        check_diff_ = run(sess,diff_eval,feed_dict = Dict(tf_variables.lambda => ones(1)*T_,tf_variables.N_k_dis=>N_k_dis_))  
+        global N_k_dis_ = update_K_p(sess,param_model_val,tf_variables,check_diff_,N_k_dis_,p_pre_soft_max)
+
+    end
+
+
+    global T_exp += 0.1
+end

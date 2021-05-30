bc34b1265b764a0c8844fadd135aca457f8532b0
dbelivanis <dbelivan@stanford.edu>
2021-05-30 20:17:04 +0100

    minor updates

--- Optimization/core_2.jl
+++ Optimization/core_2.jl
@@ -34,7 +34,7 @@ BFGS!(sess,dw_2*1e5,options=Dict("maxiter"=> maxiter, "ftol"=>1e-12, "gtol"=>1e-
 save_values(sess,param_model_val,tf_variables,q_t_x, q_t_y,p,"a")
 print_status(sess,loss,diff_eval,T_exp,T_,N_k_dis_,tf_variables)
 
-check_diff = run(sess,diff_eval,feed_dict = Dict(tf_variables.lambda => ones(1)*T_,tf_variables.N_k_dis=>N_k_dis_))
+check_diff = run(sess,diff_eval,feed_dict = Dict(tf_variables.lambda => ones(1)*T_,tf_variables.N_k_dis=>N_k_dis_),feed_dict = Dict(tf_variables.lambda => ones(1)*T_,tf_variables.N_k_dis=>64))
 
 T_exp_final =4
 

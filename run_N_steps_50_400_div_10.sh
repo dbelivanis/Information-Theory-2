a29fd751acf901f8cb94b63fac8e86e893bc4425
dbelivanis <dbelivan@stanford.edu>
2021-10-17 13:39:22 -0700

    change lambda

---
+++ run_N_steps_50_400_div_10.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name dbelivan_ADCME_init_200
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:v100:1
+#SBATCH --partition=gpu
+#SBATCH --mem=64GB
+#SBATCH --time=48:00:00
+#SBATCH --error=/home/dbelivan/research/run_N_steps_50_400_div_10.err
+#SBATCH --output=/home/dbelivan/research/run_N_steps_50_400_div_10.out
+#
+#
+echo "start script"
+module --ignore-cache load "cuda/cuda-10.1"
+nvcc --version
+#
+#
+cd Optimization
+/home/dbelivan/julia-1.5.3/bin/julia core_2.jl 400 50
+#
+echo "check end of script"
+# end script

2e6460dc063270a5b1478b131dfa9c22ac8d9300
dbelivanis <dbelivan@stanford.edu>
2022-02-24 22:16:32 -0800

    typo

--- Multi_k/multi_k_solver.jl
+++ Multi_k/multi_k_solver.jl
@@ -121,8 +121,8 @@ function solver_multi_k(model_param)
         return h_0
     end
 
-    function advance_time(h_rhs,i)
-        
+    function advance_time(h_rhs, i)
+
         h_n = TensorArray(model_param.N_k)
         j = constant(1, dtype = Int32)
 
@@ -131,37 +131,37 @@ function solver_multi_k(model_param)
         end
 
         function body(j, h_n)
-        
+
             # K_avg_x_t = tf.squeeze(tf.slice(K_avg_x, constant([1, 0, 0, 0], dtype = Int32), constant([1, -1, -1, -1], dtype = Int32)))
             # K_avg_y_t = tf.squeeze(tf.slice(K_avg_y, constant([1, 0, 0, 0], dtype = Int32), constant([1, -1, -1, -1], dtype = Int32)))
-        
+
             K_avg_x_t = K_avg_x[i][j]
             K_avg_y_t = K_avg_y[i][j]
-        
+
             K_avg_x_i = reshape(K_avg_x_t, -1) * model_param.dt / (model_param.dx)^2 / S
             K_avg_y_i = reshape(K_avg_y_t, -1) * model_param.dt / (model_param.dy)^2 / S
-        
+
             m2_m = SparseTensor(list_m2_x, list_m2_y, K_avg_x_i[list_m2_v_i], model_param.Ne, model_param.Ne)
             m3_m = SparseTensor(list_m3_x, list_m3_y, K_avg_x_i[list_m3_v_i], model_param.Ne, model_param.Ne)
             m4_m = SparseTensor(list_m4_x, list_m4_y, K_avg_y_i[list_m4_v_i], model_param.Ne, model_param.Ne)
             m5_m = SparseTensor(list_m5_x, list_m5_y, K_avg_y_i[list_m4_v_i], model_param.Ne, model_param.Ne)
-        
-        
+
+
             m2_d = SparseTensor(list_m2_x, list_m2_x, K_avg_x_i[list_m2_v_i], model_param.Ne, model_param.Ne)
             m3_d = SparseTensor(list_m3_x, list_m3_x, K_avg_x_i[list_m3_v_i], model_param.Ne, model_param.Ne)
             m4_d = SparseTensor(list_m4_x, list_m4_x, K_avg_y_i[list_m4_v_i], model_param.Ne, model_param.Ne)
             m5_d = SparseTensor(list_m5_x, list_m5_x, K_avg_y_i[list_m4_v_i], model_param.Ne, model_param.Ne)
-        
-        
+
+
             A = spdiag(model_param.Ne) - (m2_m + m3_m + m4_m + m5_m) + (m2_d + m3_d + m4_d + m5_d)
-        
+
             h_j = A \ h_rhs[j]
-                
+
             # # update head
-        
+
             h_n = write(h_n, j, h_j)
-    
-        
+
+
             j + 1, h_n
         end
 
@@ -173,9 +173,9 @@ function solver_multi_k(model_param)
     end
 
 
-    K_array = readdlm("k_array.txt");
-    K_array = reshape(check,(1,100,21,21));
-    
+    K_array = readdlm("k_array.txt")
+    K_array = reshape(K_array, (1, 100, 21, 21))
+
     rng = MersenneTwister(1234)
 
     # K_log_mean = Variable(log.(ones(1, 1, 1) * 1e-5))
@@ -229,24 +229,24 @@ function solver_multi_k(model_param)
     end
 
     function body(i, h_t_loop)
-    
+
         # calculate time
         t = cast(i - 1, Float64) * model_param.dt
-    
+
         # build rhs
         h_rhs = read(h_t_loop, i - 1)
         BC_left, BC_right = BC(t)
         h_rhs = h_rhs .* A_m[:, 1] + Array(BC_left)[:, 1] + Array(BC_right)[:, 1]
-    
+
         # advance time
-        h_next = advance_time(h_rhs,i)
+        h_next = advance_time(h_rhs, i)
         # h_next = h_rhs
         # # # update hydraulic head
         h_t_loop = write(h_t_loop, i, h_next)
-    
+
         # return head and flow
         i + 1, h_t_loop
-    
+
     end
 
     # # Define the tensor flow loop
